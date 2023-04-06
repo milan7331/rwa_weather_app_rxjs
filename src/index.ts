6317d0073045a1f9c4ac4ce53bbce5c6ba5eae17
Milan <milanstojanovski1@gmail.com>
2023-04-06 23:22:15 +0200

    project structure updated

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+
+
+
+
+const currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather";
+const weeklyForecastAPI = "https://api.openweathermap.org/data/2.5/forecast";
+const geolocationAPI = "http://api.openweathermap.org/geo/1.0/direct";
